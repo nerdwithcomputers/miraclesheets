56dda4c16d933f02bc7e3ff7854b81db3af0755d
nerdwithcomputers <hpscigeek@gmail.com>
2025-09-06 16:11:06 -0400

    tried to mess around with text styling cause i got distracted

--- lib/journal.dart
+++ lib/journal.dart
@@ -14,13 +14,34 @@ class JournalScreen extends StatefulWidget{
 
 class JournalState extends State<JournalScreen>{
   Widget buildDesc(Map input){
-    String returnString = "${input["main"]}\n\n";
+    List<InlineSpan> returns = [];
     for (MapEntry subheading in input.entries.toList()){
       if(subheading.key != "main"){
-        returnString += "${subheading.key}-\n${subheading.value}";
+        returns.add(
+          TextSpan(
+            text: " ${subheading.key}-\n",
+            style: const TextStyle(
+              fontWeight: FontWeight.w100
+            )
+          )
+        );
+        returns.add(
+          TextSpan(
+            text: "  ${subheading.value}\n",
+            style: const TextStyle(
+              fontWeight: FontWeight.w100
+            )
+          )
+        );
       }
     }
-    return Text(returnString);
+    return RichText(
+      text: TextSpan(
+        text: "${input["main"]}\n",
+        style: DefaultTextStyle.of(context).style,
+        children: returns
+      )
+    );
   }
   
   @override
@@ -31,7 +52,12 @@ class JournalState extends State<JournalScreen>{
           children: [
             ...widget.character["journal"].entries.toList().map(
               (entry)=> ListTile(
-                title: Text(entry.key),
+                title: Text(
+                  entry.key,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.w800
+                  ),
+                ),
                 subtitle: buildDesc(entry.value),
               )
             )
